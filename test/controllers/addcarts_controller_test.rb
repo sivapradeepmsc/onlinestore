b49c790ed7d1564ce83685cab5cc1a279dc8ec81
sivapradeepmsc <sivapradeep12@gmail.com>
2018-02-28 18:05:16 +0530

    First commit

--- test/controllers/addcarts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AddcartsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/bags_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BagsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
